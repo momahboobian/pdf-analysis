cdc4f63db671777bf7c60893d1f50e128209f227
Mo Mahboobian <momahboobian@gmail.com>
2024-09-09 11:15:29 +0100

    adding toast for alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
 import Home from './pages/Home/Home'
 import Admin from './pages/Admin/Admin'
 import './App.scss'
 
-const App: React.FC = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <div className="app">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
