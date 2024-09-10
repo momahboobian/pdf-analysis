c3d167efd28468909a5cdc6241588f0bd1f6708f
Mo Mahboobian <momahboobian@gmail.com>
2024-09-10 17:03:40 +0100

    Updaing port in config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4200,
+  },
   plugins: [react()],
   base: '/',
 })
